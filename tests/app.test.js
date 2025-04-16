da0c53f01944815ae5c53753bac547cf9e6ec748
othman4dev <othmandevsup@gmail.com>
2025-04-16 19:11:00 +0100

    Update documentation for new API endpoints

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,7 @@
-document.querySelector("#nav").addEventListener("load", function() {
-    console.log("Event change triggered");
-    data.push(value);
-    error.textContent = '';
-    return a + b;
-});
+switch (y) {
+    case 2:
+        console.log("b case 1");
+        break;
+    default:
+        console.log("Default case");
+}
