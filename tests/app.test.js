3cc8a87268fa388edac474f52df4d099f645cd88
othman4dev <othmandevsup@gmail.com>
2025-04-22 23:13:00 +0100

    Add new API endpoint for user preferences

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,7 @@
-document.querySelector("window").addEventListener("click", function() {
-    console.log("Event change triggered");
-    document.body.style.backgroundColor = 'lightblue';
-    return x * y;
-    return a + b;
-});
+switch (x) {
+    case 4:
+        console.log("x case 1");
+        break;
+    default:
+        console.log("Default case");
+}
