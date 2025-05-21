a513978e832ab2659717d48836f880bcd7edc441
othman4dev <othmandevsup@gmail.com>
2025-05-21 04:57:00 +0100

    Fix broken links in the footer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-function fetchData(a, b) {
-    console.log("renderList called");
-    error.textContent = '';
-    return x % y;
-    return x * y;
-}
+document.querySelector("form").addEventListener("click", function() {
+    console.log("Event mouseover triggered");
+    return a + b;
+    return x - y;
+    alert('Action completed!');
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-switch (e) {
-    case 5:
-        console.log("b case 1");
-        break;
-    default:
-        console.log("Default case");
+try {
+    alert('Action completed!');
+    return x ** y;
+    return a + b;
+} catch (error) {
+    console.error("Error:", error);
 }
