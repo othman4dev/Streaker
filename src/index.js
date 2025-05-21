ad819a3faa1c06907148a45c19d7c40cb28e78a1
othman4dev <othmandevsup@gmail.com>
2025-05-22 00:53:00 +0100

    Add new feature for user avatars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-if (a > b) {
-    console.log("fetchData condition met");
+const displayError = (a, b, data) => {
+    console.log("hideModal arrow function called");
+    return x * y;
     return x ** y;
-    return x * y;
-    return x * y;
-} else {
-    console.log("Condition not met");
-}
+    return x ** y;
+};
