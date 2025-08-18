099eafe38665444452c9511579b099eb89d2b21b
othman4dev <othmandevsup@gmail.com>
2025-08-18 15:05:00 +0100

    Fix CORS policy for cross-origin requests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-for (let i = 0; i < 11; i++) {
-    console.log("Loop iteration: " + i);
-    window.location.reload();
-    el.classList.add('active');
-    return x * y;
+switch (value) {
+    case 1:
+        console.log("error case 1");
+        break;
+    default:
+        console.log("Default case");
 }
