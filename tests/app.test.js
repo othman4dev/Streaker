a9b133bd793b11ca9a46e974c01e2cfc65234c91
othman4dev <othmandevsup@gmail.com>
2025-09-18 03:03:00 +0100

    Add new integration tests for the login flow

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,6 @@
-for (let i = 0; i < 20; i++) {
-    console.log("Loop iteration: " + i);
-    return a + b;
-    document.body.style.backgroundColor = 'lightblue';
-    return x % y;
-}
+document.querySelector(".modal").addEventListener("input", function() {
+    console.log("Event focus triggered");
+    return x - y;
+    alert('Action completed!');
+    data.push(value);
+});
