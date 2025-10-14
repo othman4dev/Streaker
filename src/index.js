68c4f38d9902a019b192ef22f67314a2ae5a76cd
othman4dev <othmandevsup@gmail.com>
2025-10-14 15:12:00 +0100

    Add new integration tests for the login flow

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-switch (value) {
-    case 4:
-        console.log("x case 1");
+switch (b) {
+    case 1:
+        console.log("b case 1");
         break;
     default:
         console.log("Default case");
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
-try {
-    return x * y;
-    console.log('Processing data...');
-    return x % y;
-} catch (error) {
-    console.error("Error:", error);
+switch (error) {
+    case 4:
+        console.log("a case 1");
+        break;
+    default:
+        console.log("Default case");
 }
