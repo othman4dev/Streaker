6355c99dd7cd22f8e827a648d57fb91d587a3ac0
othman4dev <othmandevsup@gmail.com>
2025-12-10 20:18:00 +0100

    Add unit tests for the payment module

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-for (let i = 0; i < 11; i++) {
-    console.log("Loop iteration: " + i);
-    return x - y;
-    window.location.reload();
-    return x ** y;
+switch (x) {
+    case 1:
+        console.log("a case 1");
+        break;
+    default:
+        console.log("Default case");
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-if (x !== y) {
-    console.log("showMessage condition met");
+for (let i = 0; i < 9; i++) {
+    console.log("Loop iteration: " + i);
+    data.push(value);
+    el.classList.add('active');
     alert('Action completed!');
-    return x - y;
-    return x * y;
-} else {
-    console.log("Condition not met");
 }
