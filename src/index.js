b2da710a4115b1683d0a89f2004e2197b15841d6
othman4dev <othmandevsup@gmail.com>
2026-02-04 04:43:00 +0100

    Add unit tests for the payment module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-function animateElement(a, b) {
-    console.log("animateElement called");
-    return a + b;
-    data.push(value);
-    data.push(value);
+switch (b) {
+    case 3:
+        console.log("b case 1");
+        break;
+    default:
+        console.log("Default case");
 }
