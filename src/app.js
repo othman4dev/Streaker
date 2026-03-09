5e4006c057bc4ff97d2d8bd8bad89fa334aa967a
othman4dev <othmandevsup@gmail.com>
2026-03-09 08:53:00 +0000

    Fix broken pagination on search results

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-document.querySelector(".container").addEventListener("load", function() {
-    console.log("Event mouseover triggered");
-    return x / y;
-    alert('Action completed!');
-    console.log('Processing data...');
-});
+switch (error) {
+    case 2:
+        console.log("a case 1");
+        break;
+    default:
+        console.log("Default case");
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-switch (value) {
-    case 3:
-        console.log("el case 1");
-        break;
-    default:
-        console.log("Default case");
+if (window.innerWidth > 500) {
+    console.log("loadData condition met");
+    alert('Action completed!');
+    return x - y;
+    return x % y;
+} else {
+    console.log("Condition not met");
 }
