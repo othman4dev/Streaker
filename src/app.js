84d604120cdee2d5db00b1e2ab345c1f18b55703
othman4dev <othmandevsup@gmail.com>
2026-03-20 20:42:00 +0000

    Fix broken pagination on search results

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-try {
-    alert('Action completed!');
-    window.location.reload();
-    return x - y;
-} catch (error) {
-    console.error("Error:", error);
+switch (x) {
+    case 2:
+        console.log("x case 1");
+        break;
+    default:
+        console.log("Default case");
 }
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,7 @@
-document.querySelector("header").addEventListener("submit", function() {
-    console.log("Event mouseout triggered");
-    return x - y;
-    return x - y;
-    return x ** y;
-});
+switch (e) {
+    case 5:
+        console.log("e case 1");
+        break;
+    default:
+        console.log("Default case");
+}
