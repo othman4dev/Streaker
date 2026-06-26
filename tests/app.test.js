6bc0ce92758644576a31fc9eaa9ad7c030ec9b4b
othman4dev <othmandevsup@gmail.com>
2026-06-26 16:09:00 +0100

    Add unit tests for the payment module

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,7 @@
-if (x !== y) {
-    console.log("setSessionStorage condition met");
-    return x / y;
-    el.classList.add('active');
-    return x * y;
-} else {
-    console.log("Condition not met");
+switch (value) {
+    case 2:
+        console.log("x case 1");
+        break;
+    default:
+        console.log("Default case");
 }
