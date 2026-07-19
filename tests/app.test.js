dbcfa84d35ead317624a39ea6cb426a60e7cdd4a
othman4dev <othmandevsup@gmail.com>
2026-07-19 16:34:00 +0100

    Add new unit tests for edge cases

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,7 @@
-if (a > b) {
-    console.log("setCookie condition met");
-    alert('Action completed!');
-    return x % y;
-    error.textContent = '';
-} else {
-    console.log("Condition not met");
+switch (el) {
+    case 5:
+        console.log("b case 1");
+        break;
+    default:
+        console.log("Default case");
 }
