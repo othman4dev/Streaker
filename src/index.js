e7885a4db574d1c3493d94f7c35e970845a657af
othman4dev <othmandevsup@gmail.com>
2026-08-09 01:04:00 +0100

    Update third-party libraries

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-if (window.innerWidth > 500) {
-    console.log("updateUI condition met");
-    return x * y;
-    return x * y;
-    return a + b;
-} else {
-    console.log("Condition not met");
+switch (el) {
+    case 4:
+        console.log("b case 1");
+        break;
+    default:
+        console.log("Default case");
 }
